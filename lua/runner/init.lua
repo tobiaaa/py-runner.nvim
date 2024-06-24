148b46131a006e2235863b4cffd9443933de1135
Tobias Raichle <tobias.raichle@iss.uni-stuttgart.de>
2024-06-24 17:41:16 +0200

    feat: Debug

--- lua/runner/init.lua
+++ lua/runner/init.lua
@@ -7,10 +7,6 @@ function M.setup()
     pcall(vim.fn.mkdir, save_path)
 end
 
-local run_config = function()
-    print("Running config...")
-end
-
 local save_config = function(file, config)
     local save_file = io.open(save_path .. "/configs.json", "r")
     local save_table = {}
@@ -30,10 +26,6 @@ local save_config = function(file, config)
     end
 end
 
-local run_last = function()
-    print("Running Last")
-end
-
 local ask_new_config = function()
     vim.ui.input({ prompt = "Enter configuration" },
         function(input)
@@ -41,6 +33,67 @@ local ask_new_config = function()
             save_config(filename, input)
         end)
 end
+local get_config = function(file)
+    local save_file = io.open(save_path .. "/configs.json", "r")
+    local save_table = {}
+    if save_file then
+        save_table = vim.json.decode(save_file:read("*a"))
+        save_file:close()
+    end
+    local config = save_table[file]
+    if config ~= nil then
+        return config
+    end
+    ask_new_config()
+end
+
+
+
+local get_last = function()
+    local save_file = io.open(save_path .. "/last.json", "r")
+    if not save_file then
+        -- If no saved last config, return current file
+        return vim.fn.expand('%:p')
+    end
+    local save_table = {}
+    save_table = vim.json.decode(save_file:read("*a"))
+    save_file:close()
+
+    local project = vim.fn.getcwd()
+    return save_table[project]
+end
+
+local save_last = function(config)
+    local save_file = io.open(save_path .. "/last.json", "r")
+    local save_table = {}
+    if save_file then
+        save_table = vim.json.decode(save_file:read("*a"))
+        save_file:close()
+    end
+    local project = vim.fn.getcwd
+    save_table[project] = config
+
+    save_file = io.open(save_path .. '/last.json', "w+")
+    local json = vim.json.encode(save_table)
+    if not save_file then
+        print("Could not save config")
+    else
+        save_file:write(json)
+        save_file:close()
+    end
+end
+
+local run_config = function(config)
+    print("Running config...")
+end
+
+local run_last = function()
+    local last_conf_key = get_last()
+    local last_conf = get_config(last_conf_key)
+
+    run_config(last_conf)
+end
+
 
 
 -- Create User commands

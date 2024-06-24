800950d3ac3df908a1ec056055e2041e0d0a5aea
Tobias Raichle <tobias.raichle@iss.uni-stuttgart.de>
2024-06-24 15:41:03 +0200

    feat: Debug

--- plugin/py-runner.lua
+++ plugin/py-runner.lua
@@ -9,7 +9,7 @@ local run_config = function()
 end
 
 local save_config = function(file, config)
-    local json = vim.json.encode({ file = config })
+    local json = vim.json.encode({ [file] = config })
     print(json)
 end
 
